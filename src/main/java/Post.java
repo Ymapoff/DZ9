424e6e2cc479cad34355747ef3463b21806fec28
Umarov Yuriy <kumar13-05@mail.ru>
2023-05-02 14:23:14 +0700

    initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,11 @@
+public class Post {
+
+   FormDate birthday = new FormDate();
+
+    public String name;  //имя
+    public String passport;  //паспортные данные
+    public String patronymic;  //отчество
+    public String phone;  //номер телефона
+    public String surname;  //фамилия
+    public boolean subscription;  //смс
+}
